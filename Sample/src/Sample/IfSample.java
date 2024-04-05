4cfd9a7d797b6bed1c9edac923632f386b52d826
dir-fy24ueharasora <sora.uehara.b022@dirbato.co.jp>
2024-04-05 17:39:00 +0900

    202404051738ifまで

---
+++ Sample/src/Sample/IfSample.java
@@ -0,0 +1,13 @@
+package Sample;
+
+public class IfSample {
+
+    public static void main(String[] args) {
+        var score = 75; // 成績
+        
+        if(score >= 60) {
+           System.out.println("合格です！");
+        }
+    }
+
+}
